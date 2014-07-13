2915d075150a042560273efa56eb28723b9d22a3
mgt6 <marktaycon@googlemail.com>
2014-07-13 20:42:01 +0100

    Added basic configuration for authentication

---
+++ src/main/java/com/github/mgt6/weatherstation/Application.java
@@ -0,0 +1,17 @@
+package com.github.mgt6.weatherstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
