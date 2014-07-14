2eb75e23d0e008cebf8d5c86b4240b0c8c28f34e
mgt6 <marktaycon@googlemail.com>
2014-07-14 21:36:35 +0100

    formatting fix

--- src/main/java/com/github/mgt6/weatherstation/Application.java
+++ src/main/java/com/github/mgt6/weatherstation/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
 @ComponentScan
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) throws Throwable {
