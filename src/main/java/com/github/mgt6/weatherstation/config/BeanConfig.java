b3f24341e6e65ba6bbf4a30ef9a3cc8b26894532
mgt6 <marktaycon@googlemail.com>
2014-07-14 22:54:22 +0100

    Added a sensor page to view the details of an individual sensor, todo add a get sensor method

--- src/main/java/com/github/mgt6/weatherstation/config/BeanConfig.java
+++ src/main/java/com/github/mgt6/weatherstation/config/BeanConfig.java
@@ -1,8 +1,9 @@
 package com.github.mgt6.weatherstation.config;
 
 
+import com.evrythng.java.wrapper.ApiManager;
 import com.github.mgt6.weatherstation.repository.SensorRepository;
-import com.github.mgt6.weatherstation.repository.impl.mock.MockSensorRepository;
+import com.github.mgt6.weatherstation.repository.impl.evrythng.EvrythngSensorRepository;
 import com.github.mgt6.weatherstation.service.SensorService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,13 +18,13 @@ public class BeanConfig {
         return new SensorService();
     }
 
-    @Bean
+/*    @Bean
     public SensorRepository mockSensorRepository() {
         return new MockSensorRepository();
-    }
+    }*/
 
-    /*@Bean
+    @Bean
     public SensorRepository evrythngSensorRepository() {
         return new EvrythngSensorRepository(new ApiManager("K6UxIsthLqChs6uc97SmZIEdiTHycaNiibDGSuyBox4RraMCzno3DZYWR5n6bmnJN0cAmZF12jFoZWlG"));
-    }*/
+    }
 }
--- src/main/java/com/github/mgt6/weatherstation/controller/WeatherStationController.java
+++ src/main/java/com/github/mgt6/weatherstation/controller/WeatherStationController.java
@@ -1,22 +1,32 @@
 package com.github.mgt6.weatherstation.controller;
 
+import com.github.mgt6.weatherstation.dto.SensorDto;
 import com.github.mgt6.weatherstation.service.SensorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/weatherStation")
 public class WeatherStationController {
 
     @Autowired
     private SensorService sensorService;
 
-    @RequestMapping("/weatherStation")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    @RequestMapping
+    public String sensors(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("sensors", sensorService.getSensors());
         return "weatherStation";
+    }
+
+    @RequestMapping("/sensor/{id}")
+    public String reading(@PathVariable String id, String name, Model model) {
+        model.addAttribute("sensor", new SensorDto(id, "Test", "This is a test"));
+        model.addAttribute("reading", sensorService.getLatestSensorReading(id));
+        return "sensor";
     }
 }
 
