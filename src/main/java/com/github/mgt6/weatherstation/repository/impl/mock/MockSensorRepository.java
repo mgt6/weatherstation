65dc1a04e63a7d15cdfa28c2992e86ab6893c833
mgt6 <marktaycon@googlemail.com>
2014-07-15 08:03:58 +0100

    Refactored to use the mock sensors

--- src/main/java/com/github/mgt6/weatherstation/repository/impl/mock/MockSensorRepository.java
+++ src/main/java/com/github/mgt6/weatherstation/repository/impl/mock/MockSensorRepository.java
@@ -3,54 +3,28 @@ package com.github.mgt6.weatherstation.repository.impl.mock;
 import com.evrythng.thng.resource.model.store.Property;
 import com.evrythng.thng.resource.model.store.Thng;
 import com.github.mgt6.weatherstation.domain.PropertyBuilder;
-import com.github.mgt6.weatherstation.domain.ThngBuilder;
 import com.github.mgt6.weatherstation.repository.SensorRepository;
+import com.github.mgt6.weatherstation.repository.impl.mock.data.MockRainSensor;
+import com.github.mgt6.weatherstation.repository.impl.mock.data.MockTemperatureSensor;
+import com.github.mgt6.weatherstation.repository.impl.mock.data.MockWindSensor;
 
 import java.util.*;
 
 public class MockSensorRepository implements SensorRepository {
 
-    private static Map<String, Map<String, String>> properties = new HashMap<String, Map<String, String>>();
+    private static Map<String, Map<String, Property>> properties = new HashMap<String, Map<String, Property>>();
     private static Map<String, Thng> thngsMap = new HashMap<String, Thng>();
-    private static Map<String, String> rainSensorProperties = new HashMap<String, String>();
-    private static Map<String, String> windSensorProperties = new HashMap<String, String>();
-    private static Map<String, String> temperatureSensorProperties = new HashMap<String, String>();
 
     static {
 
-        thngsMap.put("UdqcgxtgsB5RAnybdxC6BHsc", new ThngBuilder().withId("UdqcgxtgsB5RAnybdxC6BHsc")
-                .withName("Rain Sensor")
-                .withDescription("A sensor for detecting the current rate of rain fall.")
-                .withProduct("UA6cDx3pPVKaAHcbUbC6Bngn")
-                .withProperty("rainfall", "0.0")
-                .build());
+        thngsMap.put("UdqcgxtgsB5RAnybdxC6BHsc", MockRainSensor.thng);
+        thngsMap.put("Ud6yDyFTsVpaGNFEDaCN5nPg", MockWindSensor.thng);
+        thngsMap.put("UdqyXcUqPBpRf5qM7KVHQpkm", MockTemperatureSensor.thng);
 
-        thngsMap.put("Ud6yDyFTsVpaGNFEDaCN5nPg", new ThngBuilder().withId("Ud6yDyFTsVpaGNFEDaCN5nPg")
-                .withName("Wind Sensor")
-                .withDescription("A sensor to determine the current wind speed and direction")
-                .withProduct("UA6cDx3pPVKaAHcbUbC6Bngn")
-                .withProperty("windspeed", "0.9")
-                .build());
+        properties.put("UdqcgxtgsB5RAnybdxC6BHsc", MockRainSensor.properties);
+        properties.put("Ud6yDyFTsVpaGNFEDaCN5nPg", MockWindSensor.properties);
+        properties.put("UdqyXcUqPBpRf5qM7KVHQpkm", MockTemperatureSensor.properties);
 
-        thngsMap.put("UdqyXcUqPBpRf5qM7KVHQpkm", new ThngBuilder().withId("UdqyXcUqPBpRf5qM7KVHQpkm")
-                .withName("Temperature Sensor")
-                .withDescription("A Sensor to determine the current temperature")
-                .withProduct("UA6cDx3pPVKaAHcbUbC6Bngn")
-                .withProperty("temperature", "22.1")
-                .build());
-
-        rainSensorProperties.put("type", "rainfall");
-        rainSensorProperties.put("rainfall", "0.0");
-
-        windSensorProperties.put("type", "windspeed");
-        windSensorProperties.put("windspeed", "0.9");
-
-        temperatureSensorProperties.put("type", "temperature");
-        temperatureSensorProperties.put("temperature", "22.1");
-
-        properties.put("UdqcgxtgsB5RAnybdxC6BHsc", rainSensorProperties);
-        properties.put("Ud6yDyFTsVpaGNFEDaCN5nPg", windSensorProperties);
-        properties.put("UdqyXcUqPBpRf5qM7KVHQpkm", temperatureSensorProperties);
     }
 
     @Override
@@ -64,15 +38,19 @@ public class MockSensorRepository implements SensorRepository {
 
     @Override
     public Optional<Property> getLatestPropertyReading(String thngId, String propertyName) {
-        if (properties.containsKey(thngId) && properties.get(thngId).containsKey(propertyName)) {
-            return Optional.of(new PropertyBuilder().withId("2").withKey(propertyName).withValue(properties.get(thngId).get(propertyName)).build());
+        if (properties.containsKey(thngId)) {
+            Map<String, Property> sensorProperties = properties.get(thngId);
+            if (sensorProperties.containsKey(propertyName)) {
+                Property property = sensorProperties.get(propertyName);
+                return Optional.of(new PropertyBuilder().withId(property.getId()).withKey(property.getKey()).withValue(property.getValue()).build());
+            }
         }
         return Optional.empty();
     }
 
     @Override
     public Optional<Thng> getSensor(String sensorId) {
-        if(thngsMap.containsKey(sensorId)) {
+        if (thngsMap.containsKey(sensorId)) {
             return Optional.of(thngsMap.get(sensorId));
         }
         return Optional.empty();
